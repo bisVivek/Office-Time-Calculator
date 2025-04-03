a3eaa79a5db9e7e2dec9b67efb9d37d3bda32991
Vivek Bisht <vivek.bisht@ftechiz.com>
2025-04-04 00:55:01 +0530

    Initial Commit

---
+++ Time Claculator/Time_ClaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_ClaculatorApp.swift
+//  Time Claculator
+//
+//  Created by Ftechiz on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Time_ClaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Time ClaculatorTests/Time_ClaculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Time_ClaculatorTests.swift
+//  Time ClaculatorTests
+//
+//  Created by Ftechiz on 04/04/25.
+//
+
+import Testing
+@testable import Time_Claculator
+
+struct Time_ClaculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
